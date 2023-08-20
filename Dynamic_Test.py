6a328b9074f9888657a4a9f2143bb478b36d87eb
Moon-Touched <dino1997@qq.com>
2023-08-20 16:04:59 +0100

    整理代码

---
+++ Dynamic_Test.py
@@ -0,0 +1,313 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# The path to the location of Basilisk
+# Used to get the location of supporting data.
+from Basilisk import __path__
+from Basilisk.architecture import messaging, sysModel
+from Basilisk.fswAlgorithms import attTrackingError, inertial3D, rwMotorTorque
+from Basilisk.simulation import reactionWheelStateEffector, simpleNav, spacecraft, extForceTorque
+from Basilisk.utilities import SimulationBaseClass, macros, simIncludeRW, unitTestSupport, RigidBodyKinematics
+
+bskPath = __path__[0]
+fileName = os.path.basename(os.path.splitext(__file__)[0])
+
+
+class SimManager(sysModel.SysModel):
+    def __init__(self, wheelCode):
+        super(SimManager, self).__init__()
+        self.extTorqueOutMsg = messaging.CmdTorqueBodyMsg()
+        self.rwAvaiOutMsg = messaging.RWAvailabilityMsg()
+        self.wheelcode = wheelCode
+
+    def Reset(self, CurrentSimNanos):
+        return
+
+    def UpdateState(self, CurrentSimNanos):
+        extTorqueOutMsgBuffer = messaging.CmdTorqueBodyMsgPayload()
+        rwAvaiOutMsgBuffer = messaging.RWAvailabilityMsgPayload()
+
+        if CurrentSimNanos < macros.sec2nano(10.0):
+            extTorqueOutMsgBuffer.torqueRequestBody = [0, 0, 0]
+        else:
+            extTorqueOutMsgBuffer.torqueRequestBody = [0, 0, 0]
+
+        if CurrentSimNanos < macros.sec2nano(1):
+            rwAvaiOutMsgBuffer.wheelAvailability = [messaging.AVAILABLE, messaging.AVAILABLE, messaging.AVAILABLE, messaging.AVAILABLE]
+
+        else:
+            if self.wheelcode == 0:
+                rwAvaiOutMsgBuffer.wheelAvailability = [messaging.UNAVAILABLE, messaging.AVAILABLE, messaging.AVAILABLE, messaging.AVAILABLE]
+            elif self.wheelcode == 1:
+                rwAvaiOutMsgBuffer.wheelAvailability = [messaging.AVAILABLE, messaging.UNAVAILABLE, messaging.AVAILABLE, messaging.AVAILABLE]
+            elif self.wheelcode == 2:
+                rwAvaiOutMsgBuffer.wheelAvailability = [messaging.AVAILABLE, messaging.AVAILABLE, messaging.UNAVAILABLE, messaging.AVAILABLE]
+            elif self.wheelcode == 3:
+                rwAvaiOutMsgBuffer.wheelAvailability = [messaging.AVAILABLE, messaging.AVAILABLE, messaging.AVAILABLE, messaging.UNAVAILABLE]
+
+        self.extTorqueOutMsg.write(extTorqueOutMsgBuffer, CurrentSimNanos, self.moduleID)
+        self.rwAvaiOutMsg.write(rwAvaiOutMsgBuffer, CurrentSimNanos, self.moduleID)
+        return
+
+
+class Controller(sysModel.SysModel):
+    def __init__(self, attRef):
+        super(Controller, self).__init__()
+        self.Kp = 0.1
+        self.Kd = 0.1
+        self.attRef = np.array(attRef)
+        self.stateInMsg = messaging.SCStatesMsgReader()
+        self.wheelTorqueOutMsg = messaging.ArrayMotorTorqueMsg()
+        self.axisTorqueOutMsg = messaging.CmdTorqueBodyMsg()
+        self.attErrorOutMsg = messaging.AttGuidMsg()
+
+    def Reset(self, CurrentSimNanos):
+        return
+
+    def UpdateState(self, CurrentSimNanos):
+        stateInMsgBuffer = self.stateInMsg()
+        CurSigma_BN = np.array(stateInMsgBuffer.sigma_BN)
+        curOmega_BN_B = np.array(stateInMsgBuffer.omega_BN_B)
+        wheelTorqueOutMsgBuffer = messaging.ArrayMotorTorqueMsgPayload()
+        axisTorqueOutMsgBuffer = messaging.CmdTorqueBodyMsgPayload()
+        attErrorOutMsgBuffer = messaging.AttGuidMsgPayload()
+
+        attError = RigidBodyKinematics.subMRP(CurSigma_BN, -self.attRef)
+        attErrorOutMsgBuffer.sigma_BR = attError.tolist()
+
+        axisTorque = (np.array(attError) * self.Kp + np.array(curOmega_BN_B) * self.Kd).tolist()
+        # wheelTorque = np.array([0, 0, 0, 0])
+        # wheelTorque = (np.array(attError) * self.Kp + np.array(curOmega_BN_B) * self.Kd).tolist()
+
+        axisTorqueOutMsgBuffer.torqueRequestBody = axisTorque
+        wheelTorqueOutMsgBuffer.motorTorque = [0, 0, 0, 0]
+
+        # self.bskLogger.bskLog(sysModel.BSK_INFORMATION, f"{axisTorque}")
+
+        self.wheelTorqueOutMsg.write(wheelTorqueOutMsgBuffer, CurrentSimNanos, self.moduleID)
+        self.axisTorqueOutMsg.write(axisTorqueOutMsgBuffer, CurrentSimNanos, self.moduleID)
+        self.attErrorOutMsg.write(attErrorOutMsgBuffer, CurrentSimNanos, self.moduleID)
+
+        return
+
+
+def SatelliteRotation(axis, totalTime, timestep):
+    # region Simulation Parameters
+    simTaskName = "simTask"
+    simProcessName = "simProcess"
+    scSim = SimulationBaseClass.SimBaseClass()
+    simulationTime = macros.sec2nano(totalTime)
+    simulationTimeStep = macros.sec2nano(timestep)
+    # endregion
+
+    # region create the simulation process and task
+    dynProcess = scSim.CreateNewProcess(simProcessName)
+    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
+    # endregion
+
+    # region spacecraft  properties
+    scObject = spacecraft.Spacecraft()
+    scObject.ModelTag = "mySpacecraft"
+    I = [0.025, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.065]
+    scObject.hub.r_BcB_B = [[0.0], [0.0], [0.0]]
+    scObject.hub.IHubPntBc_B = unitTestSupport.np2EigenMatrix3d(I)
+    scSim.AddModelToTask(simTaskName, scObject, None, 10)
+    # endregion
+
+    # external torque
+    extTorque = extForceTorque.ExtForceTorque()
+    extTorque.ModelTag = "externalTorque"
+    scObject.addDynamicEffector(extTorque)
+    scSim.AddModelToTask(simTaskName, extTorque)
+
+    extTorqueOutMsg = messaging.CmdTorqueBodyMsg()
+    extTorqueOutMsgBuffer = messaging.CmdTorqueBodyMsgPayload()
+    if axis == "x":
+        extTorqueOutMsgBuffer.torqueRequestBody = [0.01, 0, 0]
+    elif axis == "y":
+        extTorqueOutMsgBuffer.torqueRequestBody = [0, 0.01, 0]
+    elif axis == "z":
+        extTorqueOutMsgBuffer.torqueRequestBody = [0, 0, 0.01]
+    extTorqueOutMsg.write(extTorqueOutMsgBuffer)
+
+    extTorque.cmdTorqueInMsg.subscribeTo(extTorqueOutMsg)
+
+    stateLog = scObject.scStateOutMsg.recorder()
+    scSim.AddModelToTask(simTaskName, stateLog)
+
+    scSim.InitializeSimulation()
+    scSim.ConfigureStopTime(simulationTime)
+    scSim.ShowExecutionOrder()
+    scSim.ExecuteSimulation()
+
+    return stateLog
+
+
+def TestWheelFault(wheelcode, attref, totalTime, timestep):
+    # region Simulation Parameters
+    simTaskName = "simTask"
+    simProcessName = "simProcess"
+    scSim = SimulationBaseClass.SimBaseClass()
+    simulationTime = macros.sec2nano(totalTime)
+    simulationTimeStep = macros.sec2nano(timestep)
+    # endregion
+
+    # region create the simulation process and task
+    dynProcess = scSim.CreateNewProcess(simProcessName)
+    dynProcess.addTask(scSim.CreateNewTask(simTaskName, simulationTimeStep))
+    # endregion
+
+    # region spacecraft  properties
+    scObject = spacecraft.Spacecraft()
+    scObject.ModelTag = "mySpacecraft"
+    I = [0.025, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.065]
+    scObject.hub.r_BcB_B = [[0.0], [0.0], [0.0]]
+    scObject.hub.IHubPntBc_B = unitTestSupport.np2EigenMatrix3d(I)
+    scSim.AddModelToTask(simTaskName, scObject, None, 10)
+    # endregion
+
+    # Reaction wheels
+    rwFactory = simIncludeRW.rwFactory()
+    varRWModel = messaging.BalancedWheels
+    RW1 = rwFactory.create("Honeywell_HR16", [1, 0, 0], maxMomentum=50.0, Omega=0.0, RWModel=varRWModel)
+    RW2 = rwFactory.create("Honeywell_HR16", [0, 1, 0], maxMomentum=50.0, Omega=0.0, RWModel=varRWModel)
+    RW3 = rwFactory.create("Honeywell_HR16", [0, 0, 1], maxMomentum=50.0, Omega=0.0, RWModel=varRWModel)
+    RW4 = rwFactory.create("Honeywell_HR16", [1, 1, 1], maxMomentum=50.0, Omega=0.0, RWModel=varRWModel)
+
+    numRW = rwFactory.getNumOfDevices()
+
+    #  RW  container
+    rwStateEffector = reactionWheelStateEffector.ReactionWheelStateEffector()
+    rwStateEffector.ModelTag = "RW_cluster"
+    rwFactory.addToSpacecraft(scObject.ModelTag, rwStateEffector, scObject)
+    scSim.AddModelToTask(simTaskName, rwStateEffector, None, 20)
+
+    # setup the controller
+    controller = Controller(attref)
+    controller.ModelTag = "myController"
+    scSim.AddModelToTask(simTaskName, controller, None, 1)
+
+    # Sim Manager
+    simManager = SimManager(wheelCode=wheelcode)
+    simManager.ModelTag = "SimManager"
+    scSim.AddModelToTask(simTaskName, simManager)
+
+    # external torque
+    extTorque = extForceTorque.ExtForceTorque()
+    extTorque.ModelTag = "externalTorque"
+    scObject.addDynamicEffector(extTorque)
+    scSim.AddModelToTask(simTaskName, extTorque)
+
+    rwMotorTorqueConfig = rwMotorTorque.rwMotorTorqueConfig()
+    rwMotorTorqueWrap = scSim.setModelDataWrap(rwMotorTorqueConfig)
+    rwMotorTorqueWrap.ModelTag = "rwMotorTorque"
+    scSim.AddModelToTask(simTaskName, rwMotorTorqueWrap, rwMotorTorqueConfig)
+
+    controlAxes_B = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+    rwMotorTorqueConfig.controlAxes_B = controlAxes_B
+
+    rwParamMsg = rwFactory.getConfigMessage()
+
+    # link messages
+    controller.stateInMsg.subscribeTo(scObject.scStateOutMsg)
+    rwMotorTorqueConfig.rwParamsInMsg.subscribeTo(rwParamMsg)
+    rwMotorTorqueConfig.vehControlInMsg.subscribeTo(controller.axisTorqueOutMsg)
+    rwMotorTorqueConfig.rwAvailInMsg.subscribeTo(simManager.rwAvaiOutMsg)
+    rwStateEffector.rwMotorCmdInMsg.subscribeTo(rwMotorTorqueConfig.rwMotorTorqueOutMsg)
+    # rwStateEffector.rwMotorCmdInMsg.subscribeTo(controller.wheelTorqueOutMsg)
+    extTorque.cmdTorqueInMsg.subscribeTo(simManager.extTorqueOutMsg)
+
+    stateLog = scObject.scStateOutMsg.recorder()
+    scSim.AddModelToTask(simTaskName, stateLog)
+
+    attErrorLog = controller.attErrorOutMsg.recorder()
+    scSim.AddModelToTask(simTaskName, attErrorLog)
+
+    rwMotorLog = rwMotorTorqueConfig.rwMotorTorqueOutMsg.recorder()
+    scSim.AddModelToTask(simTaskName, rwMotorLog)
+
+    cmdTorqueLog = controller.axisTorqueOutMsg.recorder()
+    scSim.AddModelToTask(simTaskName, cmdTorqueLog)
+
+    scSim.InitializeSimulation()
+    scSim.ConfigureStopTime(simulationTime)
+    scSim.ShowExecutionOrder()
+    scSim.ExecuteSimulation()
+
+    return stateLog, rwMotorLog
+
+
+def myPlot(axis, stateLog, rwMotorLog):
+    y = 0
+    if axis == "x":
+        y = 0
+    elif axis == "y":
+        y = 1
+    elif axis == "z":
+        y = 2
+    elif axis == "w":
+        y = 3
+    for idx in range(3):
+        axes[0, y].plot(timeAxis * macros.NANO2SEC, stateLog.sigma_BN[:, idx], color=unitTestSupport.getLineColor(idx, 3), label=r"$\sigma_" + str(idx + 1) + "$")
+    axes[0, y].legend(loc="lower right")
+    axes[0, y].set_xlabel("Time [s]")
+    axes[0, y].set_ylabel("Orientation MRP")
+    axes[0, y].set_title(f"{axis} rotation")
+    for idx in range(3):
+        axes[1, y].plot(timeAxis * macros.NANO2SEC, stateLog.omega_BN_B[:, idx], color=unitTestSupport.getLineColor(idx, 3), label=r"$\omega_" + str(idx + 1) + "$")
+    axes[1, y].legend(loc="lower right")
+    axes[1, y].set_xlabel("Time [s]")
+    axes[1, y].set_ylabel("angular velocity [r/s]")
+    for idx in range(4):
+        axes[2, y].plot(timeAxis * macros.NANO2SEC, rwMotorLog.motorTorque[:, idx], label=r"$T_" + str(idx + 1) + "$")
+    axes[2, y].legend(loc="lower right")
+    axes[2, y].set_xlabel("Time [s]")
+    axes[2, y].set_ylabel("Wheel torques [Nm]")
+    return
+
+
+def myPlotwheel(wheelcode, stateLog, rwMotorLog):
+    y = wheelcode - 3
+    for idx in range(3):
+        axes[0, y].plot(timeAxis * macros.NANO2SEC, stateLog.sigma_BN[:, idx], color=unitTestSupport.getLineColor(idx, 3), label=r"$\sigma_" + str(idx + 1) + "$")
+    axes[0, y].legend(loc="lower right")
+    axes[0, y].set_xlabel("Time [s]")
+    axes[0, y].set_ylabel("Orientation MRP")
+    axes[0, y].set_title(f"Wheel {wheelcode} fault")
+    for idx in range(3):
+        axes[1, y].plot(timeAxis * macros.NANO2SEC, stateLog.omega_BN_B[:, idx], color=unitTestSupport.getLineColor(idx, 3), label=r"$\omega_" + str(idx + 1) + "$")
+    axes[1, y].legend(loc="lower right")
+    axes[1, y].set_xlabel("Time [s]")
+    axes[1, y].set_ylabel("angular velocity [r/s]")
+    for idx in range(4):
+        axes[2, y].plot(timeAxis * macros.NANO2SEC, rwMotorLog.motorTorque[:, idx], label=r"$T_" + str(idx + 1) + "$")
+    axes[2, y].legend(loc="lower right")
+    axes[2, y].set_xlabel("Time [s]")
+    axes[2, y].set_ylabel("Wheel torques [Nm]")
+    return
+
+
+fig, axes = plt.subplots(nrows=3, ncols=2)
+
+# axis = "x"
+# stateLog, rwLog = TestWheelFault(0, [-0.5, -0.5, -0.5], 15, 0.01)
+# timeAxis = stateLog.times()
+# myPlotwheel(1, stateLog, rwLog)
+# axis = "y"
+# stateLog, rwLog = TestWheelFault(1, [-0.5, -0.5, -0.5], 15, 0.01)
+# myPlotwheel(2, stateLog, rwLog)
+
+
+axis = "z"
+stateLog, rwLog = TestWheelFault(2, [-0.5, -0.5, -0.5], 15, 0.01)
+timeAxis = stateLog.times()
+myPlotwheel(3, stateLog, rwLog)
+axis = "w"
+stateLog, rwLog = TestWheelFault(3, [-0.5, -0.5, -0.5], 15, 0.01)
+myPlotwheel(4, stateLog, rwLog)
+
+
+plt.tight_layout()
+plt.show()
