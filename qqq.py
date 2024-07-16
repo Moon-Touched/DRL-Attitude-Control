85d4cc6d0f67b5481154699daecf7259e5be9184
Moon-Touched <dino1997@qq.com>
2024-07-16 14:57:11 +0800

    to be rebuild

--- qqq.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-a=[ 0.42963459,  0.39684232, -0.42919071, -0.68827729]
-map = np.array([[1, 0, 0, np.sqrt(3)/3], [0, 1, 0, np.sqrt(3)/3], [0, 0, 1, np.sqrt(3)/3]])
-print(np.hstack([0,a]))
